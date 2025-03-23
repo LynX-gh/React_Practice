73886f43e2d41e7bcf08a177dbc5bb41bf035639
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-24 02:06:38 +0530

    Completed: TicTacToe with Vite

--- Tutorial/vite-tutorial/src/main.tsx
+++ Tutorial/vite-tutorial/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './assets/css/index.css'
+import './assets/css/App.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+
+createRoot(document.getElementById('header')!).render(
+  <>
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  </>
 )
